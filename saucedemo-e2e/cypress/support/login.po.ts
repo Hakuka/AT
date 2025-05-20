949868364222cec13eefa9739461c7f04cbff2d4
Hakuka <29175842+Hakuka@users.noreply.github.com>
2025-05-20 15:17:53 +0200

    Added standard user login test

--- saucedemo-e2e/cypress/support/login.po.ts
+++ saucedemo-e2e/cypress/support/login.po.ts
@@ -1,24 +1,29 @@
 const E = {
-  btnLogin:
-    '#root .login_wrapper #login_button_container .login-box [name="login-button"]',
+  btnLogin: '#root .login_wrapper #login_button_container .login-box [name="login-button"]',
+  btnOpenMenu:
+    '#root #header_container .primary_header #menu_button_container .bm-burger-button #react-burger-menu-btn',
+  btnLogout:
+    '#root #header_container .primary_header #menu_button_container .bm-menu-wrap .bm-menu .bm-item-list [data-test="logout-sidebar-link"]',
 };
 
 export class Login {
   loginToWebsite(userKey) {
     cy.fixture('users.json').then((data) => {
       const user = data[userKey];
-      cy.get(
-        '.login_wrapper #login_button_container .login-box .form_group [name="user-name"]',
-      ).type(user.username);
-      cy.get(
-        '.login_wrapper #login_button_container .login-box .form_group [name="password"]',
-      ).type(user.password);
+      cy.get('.login_wrapper #login_button_container .login-box .form_group [data-test="username"]').type(
+        user.username
+      );
+      cy.get('.login_wrapper #login_button_container .login-box .form_group [name="password"]').type(user.password);
       cy.get(E.btnLogin).should('be.visible').click();
-      cy.get(
-        '#page_wrapper #contents_wrapper #header_container .header_secondary_container [data-test="title"]',
-      )
-        .should('be.visible')
-        .should('contain', 'Products');
     });
+  }
+  loginOk() {
+    cy.get('#page_wrapper #contents_wrapper #header_container .header_secondary_container [data-test="title"]')
+      .should('be.visible')
+      .should('contain', 'Products');
+  }
+  logout() {
+    cy.get(E.btnOpenMenu).should('be.visible').click();
+    cy.get(E.btnLogout).should('be.visible').click();
   }
 }
--- saucedemo-e2e/src/e2e/login.cy.ts
+++ saucedemo-e2e/src/e2e/login.cy.ts
@@ -1,11 +1,17 @@
 import { Login } from '../../cypress/support/login.po';
 
-describe('Login to saucedemo website', () => {
+describe('Check login', () => {
   const login = new Login();
 
-  it('Open and login', () => {
+  beforeEach(() => {
+    cy.viewport(1920, 1080); //przy dodawaniu mobilnej wersji wyniesc w gorze
     cy.visit('/');
     cy.title().should('include', 'Swag Labs');
+  });
+
+  it('Check standard user', () => {
     login.loginToWebsite('standard_user');
+    login.loginOk();
+    login.logout();
   });
 });
