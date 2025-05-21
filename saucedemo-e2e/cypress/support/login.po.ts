f92e1e43bcb5cd9ed8aa0618f29a8cf308e5538e
Hakuka <29175842+Hakuka@users.noreply.github.com>
2025-05-21 11:36:25 +0200

    Added login scenario (locked user, wrong pass etc.)

--- saucedemo-e2e/cypress/support/login.po.ts
+++ saucedemo-e2e/cypress/support/login.po.ts
@@ -14,16 +14,26 @@ export class Login {
         user.username
       );
       cy.get('.login_wrapper #login_button_container .login-box .form_group [name="password"]').type(user.password);
-      cy.get(E.btnLogin).should('be.visible').click();
     });
+    cy.get(E.btnLogin).should('be.visible').click();
   }
   loginOk() {
     cy.get('#page_wrapper #contents_wrapper #header_container .header_secondary_container [data-test="title"]')
       .should('be.visible')
       .should('contain', 'Products');
   }
+  wrongLoginOrPassword() {
+    cy.get('[data-test="error"]')
+      .should('be.visible')
+      .should('contain', 'Epic sadface: Username and password do not match any user in this service');
+  }
   logout() {
     cy.get(E.btnOpenMenu).should('be.visible').click();
     cy.get(E.btnLogout).should('be.visible').click();
+  }
+  userLocked() {
+    cy.get('[data-test="error"]')
+      .should('be.visible')
+      .should('contain', 'Epic sadface: Sorry, this user has been locked out.');
   }
 }
--- saucedemo-e2e/src/e2e/login.cy.ts
+++ saucedemo-e2e/src/e2e/login.cy.ts
@@ -4,14 +4,29 @@ describe('Check login', () => {
   const login = new Login();
 
   beforeEach(() => {
-    cy.viewport(1920, 1080); //przy dodawaniu mobilnej wersji wyniesc w gorze
+    cy.viewport(1920, 1080); //przy dodawaniu mobilnej wersji wyniesc w gore
     cy.visit('/');
     cy.title().should('include', 'Swag Labs');
   });
 
-  it('Check standard user', () => {
+  it('Check standard user - login ok', () => {
     login.loginToWebsite('standard_user');
     login.loginOk();
     login.logout();
+  });
+
+  it('Check standard user - wrong password', () => {
+    login.loginToWebsite('standard_user_wrong_pass');
+    login.wrongLoginOrPassword();
+  });
+
+  it('Check standard user - wrong name', () => {
+    login.loginToWebsite('standard_user_wrong_name');
+    login.wrongLoginOrPassword();
+  });
+
+  it('Check locked out user', () => {
+    login.loginToWebsite('locked_out_user');
+    login.userLocked();
   });
 });
