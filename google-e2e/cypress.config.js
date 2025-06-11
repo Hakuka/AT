47686b621c871cc92161cd130caaadb672666040
Hakuka <29175842+Hakuka@users.noreply.github.com>
2025-06-11 21:23:06 +0200

    chore: ignore Cypress artefacts and remove old screenshots/videos/downloads from repo
    actually removed artefacts...

--- google-e2e/cypress.config.js
+++ google-e2e/cypress.config.js
@@ -3,9 +3,11 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     specPattern: 'google-e2e/src/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'google-e2e/cypress/support/e2e.js',
+    supportFile: 'google-e2e/support/e2e.js',
     fixturesFolder: 'google-e2e/cypress/fixtures',
-    downloadFolder: 'google-e2e/cypress/downloads',
+    downloadFolder: 'google-e2e/downloads',
+    screenshotsFolder: 'google-e2e/screenshots',
+    videosFolder: 'google-e2e/videos',
     baseUrl: 'https://www.google.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- saucedemo-e2e/cypress.config.js
+++ saucedemo-e2e/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     specPattern: 'saucedemo-e2e/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'saucedemo-e2e/cypress/support/e2e.js',
     fixturesFolder: 'saucedemo-e2e/cypress/fixtures',
-    downloadFolder: 'saucedemo-e2e/cypress/downloads',
+    downloadFolder: 'saucedemo-e2e/downloads',
     screenshotsFolder: 'saucedemo-e2e/screenshots',
     videosFolder: 'saucedemo-e2e/videos',
     baseUrl: 'https://www.saucedemo.com/',
