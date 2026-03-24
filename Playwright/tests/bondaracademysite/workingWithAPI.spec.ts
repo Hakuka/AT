fc37ab319098ce130bca5ae955bc1ec589347733
Hakuka <29175842+Hakuka@users.noreply.github.com>
2026-03-24 22:24:45 +0100

    add modify api response

--- Playwright/tests/bondaracademysite/workingWithAPI.spec.ts
+++ Playwright/tests/bondaracademysite/workingWithAPI.spec.ts
@@ -8,10 +8,24 @@ test.beforeEach(async ({ page }) => {
     });
   });
 
+  await page.route('*/**/api/articles*', async (route) => {
+    const response = await route.fetch();
+    const responseBody = await response.json();
+    responseBody.articles[0].title = 'This is a test title';
+    responseBody.articles[0].description = 'This is a description';
+
+    await route.fulfill({
+      body: JSON.stringify(responseBody),
+    });
+  });
+
   await page.goto('https://conduit.bondaracademy.com/');
 });
 
 test('Website is displayed', async ({ page }) => {
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
-  await page.waitForTimeout(500);
+  //  await page.waitForTimeout(500);
+
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title');
+  await expect(page.locator('app-article-list p').first()).toContainText('This is a description');
 });
