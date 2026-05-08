fcb1b98fb8c03b5c85b1a8889a910fea5c8edaff
Hakuka <29175842+Hakuka@users.noreply.github.com>
2026-05-08 22:43:25 +0200

    [WIP] hapy path scenario

--- Playwright/tests/swag-labs/purchaseHappyPath.spec.ts
+++ Playwright/tests/swag-labs/purchaseHappyPath.spec.ts
@@ -21,6 +21,8 @@ test('Purchase - happy path', async ({ page, baseURL }) => {
     await expect(page).toHaveURL('/inventory.html');
   });
 
+  //TODO: move items to test-data and use from file
+  //items with name, price
   await test.step('Add items to cart', async () => {
     await pm.onInventoryPage().addProductToCart('Sauce Labs Backpack').click();
     await pm.onInventoryPage().addProductToCart('Sauce Labs Fleece Jacket').click();
@@ -31,5 +33,25 @@ test('Purchase - happy path', async ({ page, baseURL }) => {
     await pm.onGlobalMenu().cartLink.click();
     await expect(page.getByText('Your Cart')).toBeVisible();
     await expect(pm.onInventoryPage().shoppingCartIcon).toHaveText('2');
+  });
+
+  await test.step('Verify the cart', async () => {
+    //TODO: verify: qty, prices,
+  });
+
+  await test.step('Go to the checkout', async () => {
+    //TODO: verify: qty, prices,
+  });
+
+  await test.step('Fill checkout information', async () => {
+    //TODO: use random values
+  });
+
+  await test.step('Go to the checkout overview', async () => {
+    //TODO: verify qty, prices, price total (item total)
+  });
+
+  await test.step('Finish the order and go back to home page', async () => {
+    //TODO: verify at the end that cart is empty.
   });
 });
